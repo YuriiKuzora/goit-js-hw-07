769459a423c677249a849e2e1eef348829821ad8
YuriiKuzora <yurakuzora@gmail.com>
2024-06-09 21:52:59 +0200

    task 2 end

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,15 @@
 'use strict';
 
-// const ulNumCateg = document.querySelectorAll('#categories .item');
-// console.log('Number of categories:', ulNumCateg.length);
+const ulNumCateg = document.querySelectorAll(
+  '#categories .item'
+);
+console.log('Number of categories:', ulNumCateg.length);
 
-// ulNumCateg.forEach(category => {
-//   const itemName = category.querySelector('h2').textContent;
-//   const itemElement = category.querySelectorAll('ul  li').length;
+ulNumCateg.forEach(category => {
+  const itemName = category.querySelector('h2').textContent;
+  const itemElement =
+    category.querySelectorAll('ul  li').length;
 
-//   console.log('Category:', itemName);
-//   console.log('Elements:', itemElement);
-// });
+  console.log('Category:', itemName);
+  console.log('Elements:', itemElement);
+});
